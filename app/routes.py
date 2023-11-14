a5d122bdb998770a5f61b6d1e35e32496e373b8b
Muhammed Nuhman <muhammed.nuhman@mofaic.gov.ae>
2023-11-14 16:45:17 +0400

    Add audio download to proxy handler

--- app/routes.py
+++ app/routes.py
@@ -9,25 +9,29 @@ from app import app
 def index():    
     video_qualities = [
         {
-            'title': '720p',
-            'value': '720p'
+            'title': 'Low Res',
+            'value': 'Low Res',
+        },
+        {
+            'title': '144p',
+            'value': '144p'
+        },  
+        {
+            'title': '240p',
+            'value': '240p'
+        }, 
+        {
+            'title': '360p',
+            'value': '360p'
         },
         {
             'title': '480p',
             'value': '480p'
         },
         {
-            'title': '360p',
-            'value': '360p'
-        },
-        {
-            'title': '240p',
-            'value': '240p'
-        },
-        {
-            'title': '144p',
-            'value': '144p'
-        },        
+            'title': '720p',
+            'value': '720p'
+        },    
     ]
     return render_template('index.html', video_qualities=video_qualities)
 
@@ -36,17 +40,12 @@ def download_video():
     url = request.form['ytlinktext']
     selected_quality = request.form.get('quality')  # This will get the quality selected by the user.
     audioonly = request.form.get('audioonly')
-    audioonlymp3 = request.form.get('audioonlymp3')
     yt = YouTube(url)
     
     is_mp3 = False
     if audioonly:        
         stream = yt.streams.filter(only_audio=True).order_by('abr').first()
-        print(f"Downloading audio only")
-    if audioonlymp3:        
-        stream = yt.streams.filter(only_audio=True).order_by('abr').first()
-        print(f"Downloading audio mp3 only")
-        is_mp3 = True
+        print(f"Downloading audio only")    
     elif selected_quality:
         stream = yt.streams.filter(res=selected_quality).first()
         print(f"Downloading selected resolution: {stream.resolution}")
@@ -78,8 +77,22 @@ def get_download_url():
 @app.route('/proxy', methods=['POST'])
 def proxy():
     url = request.form['ytlinktext']
+    selected_quality = request.form.get('quality')
+    audioonly = request.form.get('audioonly')
+    print("audiononlu", audioonly)
     yt = YouTube(url)
-    stream = yt.streams.get_lowest_resolution()
+
+    if audioonly:        
+        stream = yt.streams.filter(only_audio=True).order_by('abr').first()
+        print(f"Downloading audio only")
+    else:
+        if selected_quality == 'Low Res': 
+            stream = yt.streams.get_lowest_resolution()
+            print(f"Downloading lowest resolution: {stream.resolution}")    
+        else:
+            stream = yt.streams.filter(res=selected_quality).first()
+            print(f"Downloading selected resolution: {stream.resolution}")    
+    
     video_url = stream.url
 
     req = requests.get(video_url, stream=True)
